9766388bcc6df6f19e303c75150c954b86aed771
ducthanh7578 <85336545+ducthanh7578@users.noreply.github.com>
2021-06-04 14:41:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'hey bé tồ của tui!',
+    introDesc: `
+    6 tháng trước tui từ nói là lỡ t thích m 
+    thì m có block t không mà may nà chưa bị hehe`,
+    btnIntro: 'yêu',
+    title: 'ngựa là của ai',
+    desc: 'hehe ',
+    btnYes: 'của Ngân linh cẩu',
+    btnNo: 'của Cường, Như Minh,Jisoo',
+    question:'Sao hồi đó ai cũng chửi t fuckboy mà bạn vẫn iu t vại',
+    btnReply: 'trả lời thật lòng nhá',
+    reply: '',
+    mess: 'nói chung nà trả lời thế nào thì t cx iu m vãi cả l, cám ơn đã bên t suốt thời gian vừa ròi',
+    messDesc: 'Cuối tuần rảnh lúc nào kiu để t lẻn ra hun 1 cái ạ',
+    btnAccept: 'Oke',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
